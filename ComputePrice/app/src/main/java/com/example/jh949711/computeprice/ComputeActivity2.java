007f556835ac56e6d271b5d60c279a77eaa0079c
James Hund <jwhund1@buffs.wtamu.edu>
2018-02-21 21:51:12 -0600

    did some work on the add function.

--- ComputePrice/app/src/main/java/com/example/jh949711/computeprice/ComputeActivity2.java
+++ ComputePrice/app/src/main/java/com/example/jh949711/computeprice/ComputeActivity2.java
@@ -4,13 +4,39 @@ This is the class that handles after we click compute.
 This one will have the add and the list buttons active.
  */
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ComputeActivity2 extends AppCompatActivity {
 
-    EditText name, price, quantity, tax, totalPrice;
+    String name, price, quantity, tax, totalPrice;
     Button compute, add, list;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainscreen);
+        Bundle extras = getIntent().getExtras();
+        name = extras.getString("name");
+        price = extras.getString("price");
+        quantity = extras.getString("quantity");
+        tax = extras.getString("tax");
+        totalPrice = extras.getString("totalPrice");
+
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addScreen = new Intent(ComputeActivity2.this,AddActivity.class);
+
+            }
+        });
+    }
 }
